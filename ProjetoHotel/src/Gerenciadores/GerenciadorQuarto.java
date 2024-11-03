9809a25fad4ca28bb737124a6947b2043bfccfde
Natalia Viana <natalia.viana@maisunifacisa.com.br>
2024-11-03 19:25:12 -0300

    Metodos adicionados a GerenciadorQuartos

--- ProjetoHotel/src/Gerenciadores/GerenciadorQuarto.java
+++ ProjetoHotel/src/Gerenciadores/GerenciadorQuarto.java
@@ -1,18 +1,45 @@
 package Gerenciadores;
-
 import java.util.ArrayList;
 
-import SistemaMain.Quarto;
-
 public class GerenciadorQuarto {
-	private ArrayList<Quarto> listaQuartos = new ArrayList<>();
 
-	public void cadastrarQuarto(int numeroQuarto, int tipo, int capacidade, int valorDiaria) {
-		Quarto novoQuarto = new Quarto(numeroQuarto, tipo, capacidade, valorDiaria);
-		listaQuartos.add(novoQuarto);
+	import SistemaMain.Quarto;
+
+	public class GerenciadorQuarto {
+		private ArrayList<Quarto> listaQuartos = new ArrayList<>(); //lista de quartos
+
+		public void cadastrarQuarto(int numeroQuarto, int tipo, int capacidade, int valorDiaria) {//metodo cadastra novo quarto no sistema
+			Quarto novoQuarto = new Quarto(numeroQuarto, tipo, capacidade, valorDiaria);
+			listaQuartos.add(novoQuarto);
+		}
+
+		public void atualizarStatusQuarto(int numeroQuarto, String novoStatus) { //metodo que atualiza o status
+		        for (Quarto quarto : listaQuartos) {
+		            if (quarto.getNumero() == numeroQuarto) {
+		                quarto.setStatus(novoStatus);
+		                System.out.println("Status do quarto " + numeroQuarto + " atualizado para " + novoStatus);
+		                return;
+		            }
+		        System.out.println("Quarto não encontrado.");
+		    }
+		        }
+			
+		public void visualizarQuartosdisponiveis() {
+			System.out.println("Quartos Disponíveis");
+	        boolean temDisponivel = false;
+	        for (Quarto quarto : listaQuartos) {
+	            if (quarto.getStatus().equalsIgnoreCase("disponível")) {
+	                temDisponivel = true;
+	                System.out.println("Numero: " + quarto.getNumero() + 
+	                                   ", Tipo: " + quarto.getTipo() + 
+	                                   ", Capacidade: " + quarto.getCapacidade() + 
+	                                   ", Preço: " + quarto.getValorDiaria());
+	            }
+	        }
+	        if (!temDisponivel) {
+	            System.out.println("Não há quarto disponivel no momento");
+	        }
+	    }
 	}
-
-	public void atualizarStatusQuarto() {
-	}
-
 }
+
