138b1e154b9dd50aa3ce22f9f4bbf3187caec14a
Júlia Andrade <julia.andrade@maisunifacisa.com.br>
2024-11-04 21:12:34 -0300

    gerenciar de hospedes e gerenciador de hotel finlalizados

--- ProjetoHotel/src/Gerenciadores/GerenciadorHospede.java
+++ ProjetoHotel/src/Gerenciadores/GerenciadorHospede.java
@@ -1,27 +1,109 @@
-package Gerenciadores;
+package gerenciadores;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import Usuarios.Hospede;
+import excecoes.CamposNaoPreenchidosException;
+import excecoes.CpfInvalidoException;
+import interfaces.Gerenciavel;
+import tipos.Hospede;
 
 public class GerenciadorHospede {
+	Scanner sc = new Scanner(System.in);
 	private ArrayList<Hospede> listaHospedes = new ArrayList<>();
 
-	public void cadastrarHospede(String nome, String cpf, String dataNascimento, String endereco, String contato) {
-		Hospede novoHospede = new Hospede(nome, cpf, dataNascimento, endereco, contato);
-		listaHospedes.add(novoHospede);
-		System.out.println("Hospede cadastrado com sucesso");
+	public GerenciadorHospede() {
 	}
 
-	public void editarHospede(String cpf) {
-		// Implementação para editar o hóspede
+	public void cadastrar(String nome, String cpf, String dataNascimento, String endereco, String contato) {
+
+			try {
+				validacaoCpf(cpf);
+				validacaoPreencimentoDeCampos(nome, cpf, dataNascimento);
+				Hospede novoHospede = new Hospede(nome, cpf, dataNascimento, endereco, contato);
+				listaHospedes.add(novoHospede);
+				System.out.println("Novo Hóspede cadastrado com sucesso!");
+				
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		
+	}
+
+	public void editar(String cpf, String nome, String endereco, String contato) {
+		Hospede hospedeCadastrado = buscaPorCpf(cpf);
+
+		if (hospedeCadastrado != null) {
+			hospedeCadastrado.setNome(nome);
+			hospedeCadastrado.setEndereco(endereco);
+			hospedeCadastrado.setContato(contato);
+
+			System.out.println("Cadastro do hóspede " + hospedeCadastrado.getNome() + " atualizado com sucesso");
+		} else {
+			System.out
+					.println("Nenhum cadastro de hóspede com o cpf " + hospedeCadastrado.getCpf() + " foi encontrado");
+		}
+
 	}
 
 	public void buscarHospede(String cpf) {
-		// Implementação para buscar o hóspede
+		boolean isEncontrou = false;
+		for (Hospede hospede : listaHospedes) {
+			if (hospede.getCpf().equals(cpf)) {
+				isEncontrou = true;
+				System.out.println("Hóspede [Nome = " + hospede.getNome() + "| Endereço = " + hospede.getEndereco()
+						+ "| Contato  = " + hospede.getContato() + "]");
+				break;
+			}
+		}
+
+		if (!isEncontrou) {
+			System.out.println(
+					"Hóspede não encontrado, por favor verifique se as informações foram passadas corretamente e tente novamente");
+		}
 	}
 
 	public void listarHospedes() {
 
+		if (!listaHospedes.isEmpty()) {
+			System.out.println("Hóspedes Cadastrados: ");
+			for (Hospede hospede : listaHospedes) {
+				System.out.println(hospede.toString());
+			}
+		} else {
+			System.out.println("Não há hóspedes cadastrados!");
+		}
 	}
+
+	
+	//MÉTODOS ADICIONAIS
+	private Hospede buscaPorCpf(String cpf) {
+		for (Hospede hospede : listaHospedes) {
+			if (hospede.getCpf().equals(cpf)) {
+				return hospede;
+			}
+		}
+
+		return null;
+	}
+
+
+
+
+	public void validacaoCpf(String cpf) throws CpfInvalidoException {
+		if (cpf == null || !cpf.matches("\\d{11}")) {
+			throw new CpfInvalidoException("Erro: Cpf informado fora do formato válido");
+		}
+
+	}
+	public void validacaoPreencimentoDeCampos(String nome, String cpf, String dataNascimento) throws CamposNaoPreenchidosException {
+		if (nome.isBlank() || cpf.isBlank() || dataNascimento.isBlank()) { // Se algum dos campos forem verdadeiros,lança a exceçaõ
+			throw new CamposNaoPreenchidosException("Erro: Campos obrigatórios não preenchidos corretamente");
+		}
+		
+	}
+	
+	
+
 }
--- ProjetoHotel/src/SistemaMain/GerenciadorHotel.java
+++ ProjetoHotel/src/SistemaMain/GerenciadorHotel.java
@@ -1,46 +1,132 @@
-package SistemaMain;
+package br.com.terminalhotel.principal;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import excecoes.CpfInvalidoException;
+import gerenciadores.GerenciadorHospede;
+
 public class GerenciadorHotel {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
+	private static GerenciadorHospede gerenciador = new GerenciadorHospede();
+	private static Scanner scanner = new Scanner(System.in);
 
-        do {
-            System.out.println("=== Menu do Gerenciador de Hotel ===");
-            System.out.println("1. Gerenciador de Quartos");
-            System.out.println("2. Hóspedes");
-            System.out.println("3. Reservas");
-            System.out.println("4. Funcionários");
-            System.out.println("5. Sair");
-            System.out.print("Escolha uma opção: ");
-            opcao = scanner.nextInt();
+	public static void main(String[] args) {
+		int opcao;
 
-            switch (opcao) {
-                case 1:
-                    System.out.println("Você escolheu Gerenciador de Quartos.");
-                    break;
-                case 2:
-                    System.out.println("Você escolheu Hóspedes.");
-                    break;
-                case 3:
-                    System.out.println("Você escolheu Reservas.");
-                    
-                    break;
-                case 4:
-                    System.out.println("Você escolheu Funcionários.");
-                    
-                    break;
-                case 5:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
-            }
-            System.out.println();
-        } while (opcao != 5);
+		do {
+			System.out.println("=== Menu do Gerenciador de Hotel ===");
+			System.out.println("1. Gerenciador de Quartos");
+			System.out.println("2. Hóspedes");
+			System.out.println("3. Reservas");
+			System.out.println("4. Funcionários");
+			System.out.println("5. Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = scanner.nextInt();
+			scanner.nextLine();
 
-        scanner.close();
-    }
+			switch (opcao) {
+				case 1:
+					System.out.println("Você escolheu Gerenciador de Quartos.");
+					break;
+				case 2:
+					menuHospedes();
+					break;
+	
+				case 3:
+					System.out.println("Você escolheu Reservas.");
+	
+					break;
+				case 4:
+					System.out.println("Você escolheu Funcionários.");
+	
+					break;
+				case 5:
+					System.out.println("Saindo...");
+					break;
+				default:
+					System.out.println("Opção inválida! Tente novamente.");
+			}
+			System.out.println();
+		} while (opcao != 5);
+
+		scanner.close();
+
+	}
+
+
+	static private void menuHospedes() {
+		int opcao;
+		String cpf;
+		String nome;
+		String data;
+		String endereco;
+		String contato;
+		do {
+			System.out.println(">>>>>>>>>> GERENCIADOR DE HÓSPEDES <<<<<<<<<");
+			System.out.println("Opções: \n [1]Cadastrar Novo hóspede \n " + "[2]Listar Hóspedes cadastrados \n "
+					+ "[3]Pesquisar hóspede \n " + "[4]Editar cadastro de Hóspede \n " + "[5]Voltar ao menu principal");
+			System.out.println("Digite sua opção: ");
+			opcao = scanner.nextInt();
+			scanner.nextLine();
+
+			try {
+
+				switch (opcao) {
+				case 1:
+					System.out.println("Digite o cpf do hóspede: ");
+					cpf = scanner.nextLine().trim();
+
+					System.out.println("Digite o nome do hóspede: ");
+					nome = scanner.nextLine().trim();
+
+					System.out.println("Digite a data de nascimento do hóspede: ");
+					data = scanner.nextLine().trim();
+
+					System.out.println("Digite o endereço do hóspede: ");
+					endereco = scanner.nextLine().trim();
+
+					System.out.println("Digite o contato do hóspede: ");
+					contato = scanner.nextLine().trim();
+
+					gerenciador.cadastrar(nome, cpf, data, endereco, contato);
+					break;
+
+				case 2:
+					gerenciador.listarHospedes();
+					break;
+
+				case 3:
+					System.out.println("Digite o cpf do hóspede: ");
+					cpf = scanner.nextLine();
+					gerenciador.buscarHospede(cpf);
+					break;
+
+				case 4:
+					System.out.println("Digite o cpf do hóspede: ");
+					cpf = scanner.nextLine().trim();
+
+					System.out.println("Digite o nome atualizado do hóspede: ");
+					nome = scanner.nextLine().trim();
+
+					System.out.println("Digite o endereço atualizado do hóspede: ");
+					endereco = scanner.nextLine().trim();
+
+					System.out.println("Digite o contato atualizado do hóspede: ");
+					contato = scanner.nextLine().trim();
+
+					gerenciador.editar(cpf, nome, endereco, contato);
+					break;
+
+				default:
+					System.out.println("Opção Inválida, tente novamente");
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Por favor, insira um número válido.");
+				scanner.nextLine();
+			}
+
+		} while (opcao != 5);
+	
+	}
 }
